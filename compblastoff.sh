ae65bc02abb54c9599d64fc32e87c9e490cdc29a
Christopher N. Shingledecker <cns7ae@Virginia.edu>
2015-10-22 14:48:48 +0000

    updated names x 2
    git-svn-id: https://herbstresearchgroup.svn.cloudforge.com/losalamos/branches/accounting@20 547eeec1-aa83-440f-a59c-77923d1835b7

--- compblastoff.sh
+++
@@ -1,2 +0,0 @@
-#ifort -o  blastoff  chaco_data.f90 qbert.f90 moonbase.f90 parameters.f90  -march=native -O3 #0 -warn all -C 
-gfortran -o  blastoff  mc_toolbox.f03 chaco_data.f03 qbert.f03 moonbase.f03  parameters.f03 functiondefs.f03 typedefs.f03  -march=native -O0 -g  -pg -Wall -fbounds-check
