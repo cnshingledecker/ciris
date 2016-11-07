e0e4567c884999c48ab08b3da855b5698e80f91e
Christopher N. Shingledecker <cns@debian>
2016-11-07 15:18:21 -0500

    added archipelago

---
+++ archipelago/src/post.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# need to grab random seed used
+if [[ -e "seed" ]]; then
+    seed=`cat seed`
+    echo "SEED,$seed" >> "params.dat"
+fi
+
+# need to grab fitness score
+if [[ -e "fitness_results" ]]; then
+    # grabs the last line
+    tokens=( `tail -1 fitness_results` )
+    echo "Tokens='$tokens'"
+    # grabs the fitness score - last element of 4-tuple
+    fitness=${tokens[3]}
+    # $CANDIDATE should be a defined shell variable
+    # contains the name of the candidate solution's file
+    echo "FITNESS,'$fitness'"
+    if [ "$fitness" = "" ]; then
+      echo "Fitness is screwy"
+      echo "FITNESS,$tokens"
+      echo "FITNESS,$tokens" >> "params.dat"
+    else
+      echo "Fitness is okay"
+      echo "FITNESS,$fitness"
+      echo "FITNESS,$fitness" >> "params.dat"
+    fi
+else
+  echo "No fitness_results found!!"
+  echo "No fitness_results found!!" >> "params.dat"
+fi
